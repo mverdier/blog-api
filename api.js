6bff54431195d1f697dc1e405e9b8e09f5641f45
Martin <mart.verdier@gmail.com>
2017-02-05 18:18:24 +0100

    Fixed return

--- api.js
+++ api.js
@@ -83,7 +83,7 @@ app.get('/pageCount', function (req, res) {
 	blogDriver.articleAmount("articles", function(error, objs) {
 		if (error) { res.send(400, error); }
 		else { 
-			res.send(200, objs / 5);
+			res.send(200, objs);
 		}
 	});
 });
