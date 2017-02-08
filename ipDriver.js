3e08a9a98656ad0f4a317acf34ace011a061a439
Martin Verdier <mart.verdier@gmail.com>
2017-02-08 16:21:23 +0100

    Added ip hashing

--- ipDriver.js
+++ ipDriver.js
@@ -1,4 +1,5 @@
 var ObjectID = require('mongodb').ObjectID;
+var crypto = require('crypto');
 
 IpDriver = function(db) {
 	this.db = db;
@@ -17,34 +18,53 @@ IpDriver.prototype.increment = function(collectionName, address, endpoint, param
 	this.getCollection(collectionName, function(error, the_collection) {
 		if (error) callback(error);
 		else {
-			var count = {};
-
-			count['count'] = 1;
-			count['ip'] = address;
-			count['latest'] = new Date();
-			count['endpoint'] = endpoint;
-			count['param'] = param;
-
-			//Finding data for this IP address
-			the_collection.find({"ip": address, "endpoint": endpoint, "param": param}, function(error, doc) {
+			this.getCollection(collectionName, function(error, "password") {
 				if (error) callback(error);
 				else {
-
-					doc.toArray(function (err, ipCount) {
+					var secret = "";
 					
-						//If we already have a count for this IP address
-						if (ipCount.length !== 0 && ipCount[0]['count'] !== undefined) {
-							count['count'] = ipCount[0]['count'] + 1; //Updating our new value to the old one + 1
-							count['_id'] = ipCount[0]['_id']; //Matching the _id field for an update
+					//Getting the hash secret from the DB
+					password.find({"name": "ip"}, function(error, doc) {
+						if (error) callback(error);
+						else {
+							doc.toArray(function (err, pw) {
+								if (pw.length !== 0 && pw[0]['secret'] !== undefined) {
+									secret = pw[0]['secret'];
+								}
+							});	
 						}
+					})
+					
+					var count = {};
 
-						//Saving the new or updated data
-						the_collection.save(count, {w:1}, function(error, doc) {
-							if (error) callback(error);
-						});
+					count['count'] = 1;
+					count['ip'] = crypto.createHmac('sha256', secret).update(address).digest('hex');
+					count['latest'] = new Date();
+					count['endpoint'] = endpoint;
+					count['param'] = param;
+
+					//Finding data for this IP address
+					the_collection.find({"ip": address, "endpoint": endpoint, "param": param}, function(error, doc) {
+						if (error) callback(error);
+						else {
+
+							doc.toArray(function (err, ipCount) {
+
+								//If we already have a count for this IP address
+								if (ipCount.length !== 0 && ipCount[0]['count'] !== undefined) {
+									count['count'] = ipCount[0]['count'] + 1; //Updating our new value to the old one + 1
+									count['_id'] = ipCount[0]['_id']; //Matching the _id field for an update
+								}
+
+								//Saving the new or updated data
+								the_collection.save(count, {w:1}, function(error, doc) {
+									if (error) callback(error);
+								});
+							});
+						}
 					});
 				}
-			});
+			}
 		}
 	});
 };
